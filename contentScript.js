7f693432aba6cb96e1743db1f8b82e9b10f4080e
Elvis Diniz <elvisdiniz@outlook.com>
2023-09-11 00:15:50 -0300

    atualiza todos os itens ao acessar a página de julgamento

--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,22 @@ const targetHost = 'https://gestaodelicitacoes.com'
 const itemUri = /\/comprasnet-web\/seguro\/governo\/selecao-fornecedores\/item\/(\d+)$/
 const groupUri = /\/comprasnet-web\/seguro\/governo\/selecao-fornecedores\/item\/(-\d+)$/
 
+const setUpdateTime = (identificador, item) => {
+    localStorage[`${identificador}-${item}`] = new Date().getTime()
+}
+
+const getUpdateTime = (identificador, item) => {
+    return localStorage[`${identificador}-${item}`] ?? null
+}
+
+const isExpired = (identificador, item, seconds) => {
+    let updateTime = getUpdateTime(identificador, item)
+    if (!updateTime) return true
+    let now = new Date().getTime()
+    let diff = now - updateTime
+    return diff > seconds * 1000
+}
+
 const itemUpdate = (item, identificador, token) => {
     let url = `/comprasnet-fase-externa/v1/compras/${identificador}/em-selecao-fornecedores/itens/${item}/propostas`
     fetch(url, {
@@ -14,6 +30,11 @@ const itemUpdate = (item, identificador, token) => {
                 method: 'PUT',
                 body: JSON.stringify(data)
             })
+                .then(res => {
+                    if (res.status === 204) {
+                        setUpdateTime(identificador, item)
+                    }
+                })
         })
 }
 
@@ -29,6 +50,11 @@ const groupUpdate = (grupo, identificador, token) => {
                 method: 'PUT',
                 body: JSON.stringify(data)
             })
+                .then(res => {
+                    if (res.status === 204) {
+                        setUpdateTime(identificador, grupo)
+                    }
+                })
             data[0].propostasItem.forEach(proposta => {
                 let identificadorParticipante = proposta.participante.identificacao
                 let url = `/comprasnet-fase-externa/v1/compras/${identificador}/em-selecao-fornecedores/participacoes/${identificadorParticipante}/itens/${grupo}/itens-grupo/propostas`
@@ -47,6 +73,26 @@ const groupUpdate = (grupo, identificador, token) => {
         })
 }
 
+const updateAll = (identificador, token) => {
+    let url = 'https://cnetmobile.estaleiro.serpro.gov.br/comprasnet-fase-externa/v1/compras/${identificador}/itens/em-selecao-fornecedores'
+    fetch(url, {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` }
+    })
+        .then(res => res.json())
+        .then(data => {
+            data.forEach((item, i) => {
+                setTimeout(() => {
+                    if (item.tipo == 'I' && isExpired(identificador, item.numero, 120)) {
+                        itemUpdate(item.identificador, identificador, token)
+                    } else if (item.tipo == 'G' && isExpired(identificador, item.numero, 120)) {
+                        groupUpdate(item.identificador, identificador, token)
+                    }
+                }, 3 * 1000 * i);
+            })
+        })
+}
+
 let previousUrl = ''
 const observer = new MutationObserver(function (mutations) {
     if (window.location.href !== previousUrl) {
@@ -57,6 +103,9 @@ const observer = new MutationObserver(function (mutations) {
         } else if (uriData = groupUri.exec(previousUrl.split('?')[0])) {
             let urlParams = new URLSearchParams(previousUrl.split('?')[1])
             groupUpdate(uriData[1], urlParams.get('identificador'), sessionStorage['accessToken'])
+        } else if (location.href.endsWith('/comprasnet-web/seguro/governo/selecao-fornecedores')) {
+            let urlParams = new URLSearchParams(previousUrl.split('?')[1])
+            updateAll(urlParams.get('identificador'), sessionStorage['accessToken'])
         }
         previousUrl = window.location.href
     }
