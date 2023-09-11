90bac0813f10a65e87d0cf0678ecb6366a9dfa5d
Elvis Diniz <elvisdiniz@outlook.com>
2023-09-10 21:00:18 -0300

    atualiza contentScript.js agora importa dados de grupos

--- contentScript.js
+++ contentScript.js
@@ -1,38 +1,65 @@
-const updateItem = (item, identificador, token) => {
+const targetHost = 'https://gestaodelicitacoes.com'
+const itemUri = /\/comprasnet-web\/seguro\/governo\/selecao-fornecedores\/item\/(\d+)$/
+const groupUri = /\/comprasnet-web\/seguro\/governo\/selecao-fornecedores\/item\/(-\d+)$/
+
+const itemUpdate = (item, identificador, token) => {
     let url = `/comprasnet-fase-externa/v1/compras/${identificador}/em-selecao-fornecedores/itens/${item}/propostas`
     fetch(url, {
         method: 'GET',
         headers: { Authorization: `Bearer ${token}` }
     })
-        .then((res) => res.json())
+        .then(res => res.json())
         .then(data => {
+            console.log(data)
+            fetch(`${targetHost}/api/atualizar-comprasnet/${identificador}/item/${item}`, {
+                method: 'PUT',
+                body: JSON.stringify(data)
+            })
+        })
+}
 
-            if (data[0].tipo == 'I') {
-                fetch(`https://gestaodelicitacoes.com/api/atualizar-comprasnet/${identificador}/${item}`, {
-                    method: 'POST',
-                    body: JSON.stringify(data)
+const groupUpdate = (group, identificador, token) => {
+    let url = `/comprasnet-fase-externa/v1/compras/${identificador}/em-selecao-fornecedores/itens/${group}/propostas`
+    fetch(url, {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` }
+    })
+        .then(res => res.json())
+        .then(data => {
+            fetch(`${targetHost}/api/atualizar-comprasnet/${identificador}/grupo/${group}`, {
+                method: 'PUT',
+                body: JSON.stringify(data)
+            })
+            data[0].propostasItem.forEach(proposta => {
+                let identificadorParticipante = proposta.participante.identificacao
+                let url = `/comprasnet-fase-externa/v1/compras/${identificador}/em-selecao-fornecedores/participacoes/${identificadorParticipante}/itens/${group}/itens-grupo/propostas`
+                fetch(url, {
+                    method: 'GET',
+                    headers: { Authorization: `Bearer ${token}` }
                 })
-                    .then((res) => res.json())
-                    .then(data => console.log(data))
-            }
+                    .then(res => res.json())
+                    .then(data => {
+                        fetch(`${targetHost}/api/atualizar-comprasnet/${identificador}/participante/${identificadorParticipante}/grupo/${group}`, {
+                            method: 'PUT',
+                            body: JSON.stringify(data)
+                        })
+                    })
+            });
         })
 }
 
 let previousUrl = ''
 const observer = new MutationObserver(function (mutations) {
     if (window.location.href !== previousUrl) {
-        previousUrl = window.location.href
-        if (location.href.includes('https://cnetmobile.estaleiro.serpro.gov.br/comprasnet-web/seguro/governo/selecao-fornecedores/item/')) {
-
-            if (location.href.includes('itens-grupo')) {
-                return
-            }
-
-            let item = location.href.split('item/')[1].split('?')[0]
-            let queryParam = location.href.split('?')[1];
-            let urlParams = new URLSearchParams(queryParam)
-            updateItem(item, urlParams.get('identificador'), sessionStorage['accessToken'])
+        let uriData = null
+        if (uriData = itemUri.exec(previousUrl.split('?')[0])) {
+            let urlParams = new URLSearchParams(previousUrl.split('?')[1])
+            itemUpdate(uriData[1], urlParams.get('identificador'), sessionStorage['accessToken'])
+        } else if (uriData = groupUri.exec(previousUrl.split('?')[0])) {
+            let urlParams = new URLSearchParams(previousUrl.split('?')[1])
+            groupUpdate(uriData[1], urlParams.get('identificador'), sessionStorage['accessToken'])
         }
+        previousUrl = window.location.href
     }
 });
 const config = { subtree: true, childList: true }
